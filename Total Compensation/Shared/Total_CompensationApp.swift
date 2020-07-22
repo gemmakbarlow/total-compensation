8d5f0d34dfb3c1ffa7a8743b641e2abaa7c9241f
Gemma Barlow <gemma@goradar.com>
2020-07-22 13:16:27 -0400

    Initial project setup, including ‘Equity’ widget

---
+++ Total Compensation/Shared/Total_CompensationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Total_CompensationApp.swift
+//  Shared
+//
+//  Created by Gemma Barlow on 7/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Total_CompensationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
